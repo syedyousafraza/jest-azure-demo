b50729f029bc4801205be82395d53749a36c0f80
unknown <syedyousafraza@yahoo.com>
2024-11-01 13:16:23 +0500

    azure pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,24 @@
 module.exports = {
-    clearMocks: true,
-    testEnvironment: "node",
-    
-    testMatch: [
-      "**/__tests__/**/*.js",
-      "**/*.test.js",
-      "**/*.spec.js"
-    ],
-  
-    // Allow empty test suites
-    passWithNoTests: true,
-    
-    // Built-in coverage reporting
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageReporters: [
-      "text",
-      "text-summary",
-      "html",
-      "lcov", 
-      "cobertura", 
-    ],
-  
-    verbose: true
-  };
+  clearMocks: true,
+  testEnvironment: "node",
+  testMatch: [
+    "**/__tests__/**/*.js",
+    "**/*.test.js",
+    "**/*.spec.js"
+  ],
+  passWithNoTests: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: [
+    "text",
+    "text-summary",
+    "html",
+    "lcov", 
+    "cobertura",
+  ],
+  reporters: [
+    'default',
+    [ 'jest-junit', { outputDirectory: './', outputName: 'jest-test-results.xml' } ]
+  ],
+  verbose: true
+};
